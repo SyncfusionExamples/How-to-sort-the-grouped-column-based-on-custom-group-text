e430c4420bc2988243b9fe1af8fc887666684f7d
ChidanandMurugaiah <chidanand.murugaiah@syncfusion.com>
2024-02-09 15:51:36 +0530

    Sample added

---
+++ SfDataGrid_Net7_0/Helper/CustomComparer.cs
@@ -0,0 +1,71 @@
+﻿using SfDataGrid_Net7_0.Model;
+using Syncfusion.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfDataGrid_Net7_0
+{
+    public class CustomComparer : IComparer<object>, ISortDirection
+    {
+        public int Compare(object x, object y)
+        {
+            DateTime nameX = new DateTime();
+            DateTime nameY = new DateTime();
+
+            //While data object passed to comparer
+
+            if (x.GetType() == typeof(SalesByDate))
+            {
+                nameX = ((SalesByDate)x).Date;
+                nameY = ((SalesByDate)y).Date;
+            }
+
+            //While sorting groups
+
+            else if (x.GetType() == typeof(Group))
+            {
+
+                //Calculating the group key length
+                nameX = ((((Group)x).Records[0] as RecordEntry).Data as SalesByDate).Date;
+                nameY = ((((Group)y).Records[0] as RecordEntry).Data as SalesByDate).Date;
+            }
+
+            //else
+            //{
+            //    nameX = x.ToString().Length;
+            //    nameY = y.ToString().Length;
+            //}
+
+            //returns the comparison result based in SortDirection.
+
+            if (nameX < nameY)
+                return SortDirection == ListSortDirection.Ascending ? 1 : -1;
+
+            else if (nameX > nameY)
+                return SortDirection == ListSortDirection.Ascending ? -1 : 1;
+
+            else
+                return 0;
+        }
+
+
+        private ListSortDirection _SortDirection;
+
+        /// <summary>
+        /// Gets or sets the property that denotes the sort direction.
+        /// </summary>
+        /// <remarks>
+        /// SortDirection gets updated only when sorting the groups. For other cases, SortDirection is always ascending.
+        /// </remarks>
+
+        public ListSortDirection SortDirection
+        {
+            get { return _SortDirection; }
+            set { _SortDirection = value; }
+        }
+    }
+}
---
+++ SfDataGrid_Net7_0/ViewModel/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using SfDataGrid_Net7_0.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfDataGrid_Net7_0
+{
+    class SalesInfoViewModel
+    {
+        public SalesInfoViewModel()
+        {
+
+        }
+
+        private ObservableCollection<SalesByDate> _DailySalesDetails = null;
+
+        public ObservableCollection<SalesByDate> DailySalesDetails
+        {
+            get
+            {
+                if (_DailySalesDetails == null)
+                    return new SalesInfoRepository().GetSalesDetailsByDay(60);
+                else
+                    return _DailySalesDetails;
+            }
+
+        }
+    }
+}
